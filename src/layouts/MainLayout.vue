95bf252f9ace0fbc4211b0c7ca0fbe8a55e40f4e
Kaveesha Subashana <kaveeshasubashana@gmail.com>
2025-05-12 12:12:20 +0530

    layout and slider

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,7 +4,7 @@
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title> my fiest app </q-toolbar-title>
+        <q-toolbar-title> my frist app </q-toolbar-title>
 
         <div>version 1.0.0</div>
       </q-toolbar>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,59 @@
 <template>
-  <q-page class="flex">
-    <h1>Home page</h1>
+  <q-page class="">
+    <q-carousel swipeable animated v-model="slide" :autoplay="autoplay" ref="carousel" infinite>
+      <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
+      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
+      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+
+      <template v-slot:control>
+        <q-carousel-control
+          position="top-right"
+          :offset="[18, 18]"
+          class="text-white rounded-borders"
+          style="background: rgba(0, 0, 0, 0.3); padding: 4px 8px"
+        >
+          <q-toggle dense dark color="orange" v-model="autoplay" label="Auto Play" />
+        </q-carousel-control>
+
+        <q-carousel-control position="bottom-right" :offset="[18, 18]" class="q-gutter-xs">
+          <q-btn
+            push
+            round
+            dense
+            color="orange"
+            text-color="black"
+            icon="arrow_left"
+            @click="$refs.carousel.previous()"
+          />
+          <q-btn
+            push
+            round
+            dense
+            color="orange"
+            text-color="black"
+            icon="arrow_right"
+            @click="$refs.carousel.next()"
+          />
+        </q-carousel-control>
+      </template>
+    </q-carousel>
   </q-page>
 </template>
 
-<script setup>
-//
+<script>
+import { ref, defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  setup() {
+    const slide = ref(1)
+    const autoplay = ref(false)
+
+    return {
+      slide,
+      autoplay,
+    }
+  },
+})
 </script>
