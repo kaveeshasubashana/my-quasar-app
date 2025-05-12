3d3fb882d11dad05b0035c6e0171f9390c23960b
Kaveesha Subashana <kaveeshasubashana@gmail.com>
2025-05-12 09:00:56 +0530

    about page

---
+++ src/pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="q-pa-md">
+    <h1>About Page</h1>
+    <p>This is the About page of the application.</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage',
+}
+</script>
+
+<style></style>
